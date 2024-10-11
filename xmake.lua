5b06e31a0a1ede45c57b3bfa70894365f231bc95
odorajbotoj <odorajbotoj@163.com>
2024-10-11 22:48:35 +0800

    set kind = headeronly

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 add_rules("mode.debug", "mode.release")
 
 target("timewheel")
-    set_kind("library", { headeronly = true })
+    set_kind("headeronly")
     add_files("src/*.h")
     add_headerfiles("src/**.h")
     add_includedirs("src")
