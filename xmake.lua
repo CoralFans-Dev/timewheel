d89ee59125378d36de071cecc7c416dad57557f0
odorajbotoj <odorajbotoj@163.com>
2024-10-11 22:47:40 +0800

    update kind

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,9 @@
 add_rules("mode.debug", "mode.release")
 
 target("timewheel")
-    set_kind("headeronly")
+    set_kind("library", { headeronly = true })
     add_files("src/*.h")
     add_headerfiles("src/**.h")
     add_includedirs("src")
+    set_languages("c++20")
+    set_plat()
